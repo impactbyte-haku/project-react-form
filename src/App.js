3625bf0d8145f8841dda8b9c93be1cd0dafddb39
M Haidar Hanif <mhaidarhanif@gmail.com>
2019-04-25 15:02:15 +0700

    Install emotion and formik

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import { MyForm } from './components/MyForm'
 import { FormRegister } from './components/FormRegister'
-import { Footer } from './components/Footer'
+import { FormBeasts } from './components/FormBeasts'
+import { FormikForm } from './components/FormikForm'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  text-align: center;
+`
+
+const Main = styled.main`
+  flex-grow: 1;
+`
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Header />
 
-      <main>
+      <Main>
         <MyForm />
         <FormRegister />
-      </main>
+        <FormBeasts />
+        <FormikForm />
+      </Main>
 
       <Footer />
-    </div>
+    </Container>
   )
 }
 
